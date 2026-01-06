abef3afb0c5adb470838234cfb2936461ae6c28c
pwintergerst <96541888+pwintergerst@users.noreply.github.com>
2026-01-06 17:51:24 +0100

    Add DS18B20 temperature sensor code

---
+++ ds18b20.ino
@@ -0,0 +1,30 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 3
+
+// création des objets oneWire et sensors
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+// Debut du loop
+void loop() {
+  // demande les températures sur le 1-Wire
+  sensors.requestTemperatures();
+  // récupere la température sur le premier capteur qui répond
+  float temp = sensors.getTempCByIndex(0);
+
+  // affichage du temps de la mesure en seconde
+  unsigned long temps = millis()/1000;
+
+  if (temp != DEVICE_DISCONNECTED_C) {
+    Serial.print(temps);
+    Serial.print(",");
+    Serial.println(temp);
+  }
+}
